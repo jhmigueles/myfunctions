ad286c8a9c0cc0dc0783572b38c8e650a5e5fef0
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-10-25 11:34:28 +0200

    improved output from explore

--- R/explore.R
+++ R/explore.R
@@ -4,13 +4,15 @@
 #' @param dat Dataset
 #' @param idvar Subject identification variable
 #' @param outputdir Directory to store the output
+#' @param outputfilename Desired tagname for the excel and the pdf files generated
 #'
 #' @return Nothing, it saves a csv and pdf file with descriptives and visualizations
 #' @export
 #'
 #' @importFrom utils txtProgressBar setTxtProgressBar
 #' @importFrom car qqPlot Boxplot
-explore = function(dat = c(), idvar = c(), outputdir = "./"){
+#' @importFrom openxlsx write.xlsx
+explore = function(dat = c(), idvar = c(), outputdir = "./", outputfilename = "data"){
 
   if (is.character(dat)) {
     if(grep(".csv", dat, fixed = TRUE) == 1) {
@@ -26,42 +28,36 @@ explore = function(dat = c(), idvar = c(), outputdir = "./"){
   library(car)
 
   #Cleaning potential extra rows
-  dat.tmp = dat[which(is.na(dat[,idvar])==F),]
-
-  #Subsetting only numeric variables
-  # numstmp=c()
-  # for (i in 1:ncol(dat.tmp)) numstmp[i]=length(unique(dat.tmp[,i]))
-  # nums = which(numstmp > 4)
-  # 
-  # dat = dat.tmp[,nums]
+  dat.tmp = dat[which(is.na(dat[,idvar]) == F),]
 
   nums = which(sapply(dat, is.numeric))
   dat = dat[,nums]
   rm(dat.tmp)
 
   #Output directory
-  if(dir.exists(outputdir)==F) dir.create(outputdir)
+  if (dir.exists(outputdir) == F) dir.create(outputdir)
 
   # Progess bar
-  pb <- txtProgressBar(min = 0, max = ncol(dat)-1, style = 3)
+  pb <- txtProgressBar(min = 0, max = ncol(dat) - 1, style = 3)
 
 
   #QQ-plots
-  pdf(file.path(outputdir, "distribution plots.pdf"),
+  filename_pdf = ifelse(length(outputfilename > 0), paste0("_", outputfilename, ".pdf"), "")
+  pdf(file.path(outputdir, paste0("distribution", filename_pdf)),
       paper = "a4", width = 7, height = 10)
 
-  for (i in 2:ncol(dat)){
+  for (i in 2:ncol(dat)) {
     layout(rbind(c(1, 1), c(2, 3)))
     hist.default(dat[,i], main = colnames(dat[i]))
-    qqPlot(dat[,i], envelope = F, ylab = "Observed value")
-    Boxplot(dat[,i], ylab = colnames(dat)[i])
-    if(!("desc" %in% ls())){
-      desc = as.data.frame(matrix(NA, nrow = ncol(dat)-1, ncol = 12))
+    car::qqPlot(dat[,i], envelope = F, ylab = "Observed value")
+    car::Boxplot(dat[,i], ylab = colnames(dat)[i])
+    if (!("desc" %in% ls())) {
+      desc = as.data.frame(matrix(NA, nrow = ncol(dat) - 1, ncol = 12))
       colnames(desc) = c("variable","n", "missing", "mean", "sd", "min", "q1", "median", "q3", "max",
                          "outliers", "extreme outliers")
     }
 
-    desc[i-1,] = c(colnames(dat)[i],
+    desc[i - 1,] = c(colnames(dat)[i],
                    sum(!is.na(dat[,i])),
                    sum(is.na(dat[,i])),
                    mean(dat[,i], na.rm = T),
@@ -72,6 +68,12 @@ explore = function(dat = c(), idvar = c(), outputdir = "./"){
     setTxtProgressBar(pb, i)
   }
   dev.off()
-  write.csv(desc, file.path(outputdir, "descriptives.csv"), row.names = F)
+  # save to excel file
+  filename_xlsx = ifelse(length(outputfilename > 0), paste0("_", outputfilename, ".xlsx"), "")
+  openxlsx::write.xlsx(x = desc, file = file.path(outputdir, filename_xlsx),
+                       asTable = T, overwrite = TRUE,
+                       creator = "Jairo Hidalgo Migueles", sheetName = "Descriptives",
+                       firstRow = TRUE, firstCol = TRUE, colWidths = "auto", na.string = " ")
+  
   print(paste(i, "variables explored"))
 }
